f35983efb7507e3f9997e56a4db6b19e548119f0
ishanchaskar <91770647+ishanchaskar@users.noreply.github.com>
2022-07-26 16:57:53 +0530

    fourth

--- Main.py
+++ Main.py
@@ -1,38 +1,59 @@
-import os
 class Stack:
     def __init__(self, size):
-        self.items = []
+        self.items = [] ;
         self.size = size
-        self.top=-1
+        self.indx=-1
 
     def is_empty(self):
-        if (self.top==-1):
-            return 1
-        else:
-            return 0
+        """
+        Check The List items if it is empty.
+        Returns true if empty false if not.
+        """
+        # Write code here
+        if stack.indx == -1:
+            return True
+        return False
+
+
 
     def is_full(self):
-        if self.top == (self.size - 1) :
-            return 1
-        else :
-            return 0
+        """
+        Check if The stack is full.
+        Returns true if full
+        returns false if not full.
+        """
+        # Write code here
+        if stack.indx== stack.size-1 :
+            return True
+        return False
+
     def push(self, data):
-         if self.isFull()==1:
-                print("The stack is full")              
-                return "Push is not allowed"
-        else:
-                self.top = self.top+1
-                self.l[self.top]=data
+        """
+        Pushes value to stack if stack is empty. 
+        
+        Args:
+        data: The value to be pushed.
+        """
+        if not self.is_full():
+            # Write code here
+            stack.items.append(data)
+            stack.indx=stack.indx+1
+
     def pop(self):
-        if self.isEmpty()==1:
-            return "the stack is empty"
-        else:
-            print(self.l[self.top])
-            self.top = self.top-1
-            return "Array removed"
+        """ 
+        Pushes the top value out of stack.
+        """
+        if not self.is_empty():
+            # Write code here
+            stack.items.pop()
+            stack.indx=stack.indx-1
+
     def status(self):
-        for x in range(self.top+1):
-          print(self.l[x])
+        # Write code here
+        for i in range(0, len(stack.items)):
+             print(stack.items[i],end="\n")
+             
+
 # Do not change the following code
 size, queries = map(int, input().rstrip().split())
 stack = Stack(size)
